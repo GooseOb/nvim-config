55cd1d7f0b7530234e9f4f1ee750588e020166ab
GooseOb <gooose.ob@gmail.com>
2024-05-27 18:42:41 +0200

    combine some mappings into one statement

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -6,17 +6,13 @@ vim.keymap.set("n", "<leader>E", ":Neotree left reveal<CR>")
 vim.keymap.set("n", "<leader>o", ":Neotree float git_status<CR>")
 
 -- Navigation
-vim.keymap.set("n", "<a-up>", ":wincmd k<CR>")
-vim.keymap.set("t", "<a-up>", "<cmd>:wincmd k<CR>")
+vim.keymap.set({ "n", "t" }, "<a-up>", "<cmd>:wincmd k<CR>")
 
-vim.keymap.set("n", "<a-down>", ":wincmd j<CR>")
-vim.keymap.set("t", "<a-down>", "<cmd>:wincmd j<CR>")
+vim.keymap.set({ "n", "t" }, "<a-down>", "<cmd>:wincmd j<CR>")
 
-vim.keymap.set("n", "<a-left>", ":wincmd h<CR>")
-vim.keymap.set("t", "<a-left>", "<cmd>:wincmd h<CR>")
+vim.keymap.set({ "n", "t" }, "<a-left>", "<cmd>:wincmd h<CR>")
 
-vim.keymap.set("n", "<a-right>", ":wincmd l<CR>")
-vim.keymap.set("t", "<a-right>", "<cmd>:wincmd l<CR>")
+vim.keymap.set({ "n", "t" }, "<a-right>", "<cmd>:wincmd l<CR>")
 
 -- Splits
 vim.keymap.set("n", "|", ":v tsplit<CR>")
@@ -32,8 +28,7 @@ local primary_term = vertical_term
 vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
 vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
 vim.keymap.set("n", "<leader>tv", vertical_term)
-vim.keymap.set("n", "<a-t>", primary_term)
-vim.keymap.set("t", "<a-t>", "<cmd>" .. primary_term)
+vim.keymap.set({ "n", "t" }, "<a-t>", "<cmd>" .. primary_term)
 
 -- Command shortcuts
 vim.keymap.set("n", "gs", ":Gitsigns")
@@ -41,8 +36,7 @@ vim.keymap.set("n", "gs", ":Gitsigns")
 -- Other
 vim.keymap.set("n", "<leader>/", ":CommentToggle<CR>")
 
-vim.keymap.set("i", "<c-s>", "<cmd>:w<CR>")
-vim.keymap.set("n", "<c-s>", ":w<CR>")
+vim.keymap.set({ "n", "i" }, "<c-s>", "<cmd>:w<CR>")
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 
 vim.keymap.set("n", "<leader>x", ":BufferLinePickClose<CR>")
