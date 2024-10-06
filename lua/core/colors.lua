deff4e193b9d54e6cf489a6e95afcc34589cab5d
GooseOb <gooose.ob@gmail.com>
2024-10-06 21:37:52 +0200

    refactor colors

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,15 +1,32 @@
 vim.opt.termguicolors = true
 
+local to_migrate = {
+	["onedark"] = true,
+}
+
+local to_make_bg_dark = {
+	["onedark"] = true,
+}
+
 function SetColor(color)
+	if string.find(color, "kanagawa") then
+		require("kanagawa").setup({
+			keywordStyle = { italic = false },
+		})
+	end
+
 	vim.cmd.colorscheme(color)
 
-	if color == "onedark" then
+	if to_migrate[color] then
+		require("helpers.treesitter-migrate")
+	end
+
+	if to_make_bg_dark[color] then
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
 		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-		require("helpers.treesitter-migrate")
 	end
 end
 
-SetColor("kanagawa-dragon")
+SetColor("kanagawa-wave")
