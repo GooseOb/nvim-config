11b65461f44aa2ac7de6cfee1321e104d78a27b5
GooseOb <gooose.ob@gmail.com>
2024-10-06 16:42:29 +0200

    rearrange plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -12,8 +12,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	-- Snippets
 	{ "hrsh7th/vim-vsnip" },
 	{ "hrsh7th/vim-vsnip-integ" },
+	-- File tree
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		cmd = "Neotree",
@@ -25,56 +27,63 @@ require("lazy").setup({
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 	},
+	-- Treesitter
 	{
 		"nvim-treesitter/nvim-treesitter",
 		event = { "BufReadPost", "BufNewFile" },
 	},
+	{ "windwp/nvim-ts-autotag" }, -- Auto close tags
+	-- LSP
 	{ "neovim/nvim-lspconfig" },
+	{ "jose-elias-alvarez/null-ls.nvim" }, -- archived, but still works
 	-- Color schemes
 	{ "joshdick/onedark.vim" },
 	{ "catppuccin/nvim", name = "catppuccin" },
 	{ "rebelot/kanagawa.nvim", lazy = false },
+	-- Completion
 	{ "hrsh7th/cmp-nvim-lsp" },
 	{ "hrsh7th/cmp-buffer" },
 	{ "hrsh7th/cmp-path" },
 	{ "hrsh7th/cmp-cmdline" },
 	{ "hrsh7th/nvim-cmp" },
+	-- Copilot
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+	},
+	{ "zbirenbaum/copilot-cmp" },
 	{
 		"williamboman/mason.nvim",
 		build = ":MasonUpdate",
 	},
+	-- Git
+	{ "lewis6991/gitsigns.nvim" },
+	-- Telescope
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
 	},
-	{ "jose-elias-alvarez/null-ls.nvim" },
-	{ "windwp/nvim-ts-autotag" },
-	{
-		"akinsho/bufferline.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-	{
-		"terrortylor/nvim-comment",
-		keys = { "gc", "gcc", "gbc" },
-	},
-	{ "lewis6991/gitsigns.nvim" },
-	{ "folke/which-key.nvim" },
+	-- Terminal
 	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		config = true,
 		cmd = "ToggleTerm",
 	},
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-	},
+	-- File icons
 	{ "onsails/lspkind.nvim" },
+	-- Other
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{ "folke/which-key.nvim" },
+	{
+		"terrortylor/nvim-comment",
+		keys = { "gc", "gcc", "gbc" },
+	},
 	{
 		"kylechui/nvim-surround",
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
