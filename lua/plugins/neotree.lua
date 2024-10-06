2fae99244b53d00bac64177f5b5f4290e5883327
GooseOb <gooose.ob@gmail.com>
2024-10-07 01:57:50 +0200

    remap "d" in neotree to sych files removal with git

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,4 +2,21 @@ vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticS
 vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
-require("neo-tree").setup({})
+require("neo-tree").setup({
+	window = {
+		mappings = {
+			["d"] = function(state)
+				local path = state.tree:get_node().path
+				local is_tracked = vim.fn.systemlist("git ls-files " .. path)[1]
+				if is_tracked then
+					vim.fn.system("git rm -f " .. path)
+					print("Staged deleted file: " .. path)
+				else
+					vim.fn.delete(path)
+					print("Deleted file: " .. path)
+				end
+				require("neo-tree.sources.manager").refresh(state)
+			end,
+		},
+	},
+})
