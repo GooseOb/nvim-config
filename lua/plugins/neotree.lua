b2141c90083f6c0c111658a0cfe0558ed396ce65
GooseOb <gooose.ob@gmail.com>
2024-10-07 17:08:40 +0200

    fix file removal git synch

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,21 +3,16 @@ vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSi
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 require("neo-tree").setup({
-	window = {
-		mappings = {
-			["d"] = function(state)
-				local path = state.tree:get_node().path
-				local confirm = vim.fn.input("Are you sure you want to delete '" .. path .. "'? (y/n): ")
-				if confirm:lower() == "y" then
-					local is_tracked = vim.fn.systemlist("git ls-files " .. path)[1]
+	event_handlers = {
+		{
+			event = "file_deleted",
+			handler = function(file_path)
+				if vim.fn.filereadable(file_path) == 0 then
+					local is_tracked = vim.fn.systemlist("git ls-files " .. file_path)[1]
 					if is_tracked then
-						vim.fn.system("git rm -f " .. path)
-						print("Staged deleted file: " .. path)
-					else
-						vim.fn.delete(path)
-						print("Deleted file: " .. path)
+						vim.fn.system("git rm " .. file_path)
+						print("Staged deleted file: " .. file_path)
 					end
-					require("neo-tree.sources.manager").refresh(state)
 				end
 			end,
 		},
