5d476e78243f4924f1a6c63bf2844b85f7e72f2f
GooseOb <gooose.ob@gmail.com>
2024-10-09 23:24:37 +0200

    add mason packages list

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,1 +1,30 @@
-require("mason").setup()
+local mason = require("mason")
+mason.setup()
+
+local mason_registry = require("mason-registry")
+
+local function ensure_installed(packages)
+	for _, package in ipairs(packages) do
+		local p = mason_registry.get_package(package)
+		if not p:is_installed() then
+			p:install()
+		end
+	end
+end
+
+ensure_installed({
+	"bash-language-server",
+	"clangd",
+	"emmet-ls",
+	"eslint_d",
+	"google-java-format",
+	"html-lsp",
+	"jdtls",
+	"lua-language-server",
+	"prettier",
+	-- "prettierd",
+	"rust-analyzer",
+	"stylua",
+	"typescript-language-server",
+	"vue-language-server",
+})
