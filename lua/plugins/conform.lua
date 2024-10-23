e5ad2b7615a7b179cc6dfd4cef96d609a607116f
GooseOb <gooose.ob@gmail.com>
2024-10-23 02:15:19 +0200

    complete migration from null-ls to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,10 +1,37 @@
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		rust = { "rustfmt", lsp_format = "fallback" },
-		javascript = { "prettier" },
-		typescript = { "prettier" },
+local formatters_by_ft = {
+	lua = { "stylua" },
+	rust = { "rustfmt" },
+	java = { "google-java-format" },
+}
+
+local ft_by_formatter = {
+	prettier = {
+		"css",
+		"vue",
+		"javascript",
+		"typescript",
+		"scss",
+		"less",
+		"html",
+		"json",
+		"jsonc",
+		"yaml",
+		"markdown",
+		"markdown.mdx",
+		"graphql",
+		"handlebars",
 	},
+}
+
+for formatter, fts in pairs(ft_by_formatter) do
+	local formatters = { formatter }
+	for _, ft in ipairs(fts) do
+		formatters_by_ft[ft] = formatters
+	end
+end
+
+require("conform").setup({
+	formatters_by_ft = formatters_by_ft,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
