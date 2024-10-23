91d6bd9c4d4422d1695596765c90ca7594248fc8
GooseOb <gooose.ob@gmail.com>
2024-10-23 19:48:09 +0200

    reorder mappings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,18 +1,24 @@
 local wk = require("which-key")
 
 wk.add({
-	{ "<leader>/", group = "Comment" },
+	{ "<leader>/", desc = "Comment" },
 	{ "<leader>e", desc = "File Manager" },
+	{ "<leader>h", desc = "No highlight" },
+	{ "<leader>o", desc = "Git Status" },
+	{ "<leader>w", desc = "Save" },
+	{ "<leader>q", desc = "Close Current Buffer" },
+
 	{ "<leader>f", group = "Find" },
 	{ "<leader>fb", desc = "Find Buffer" },
 	{ "<leader>ff", desc = "Find File" },
 	{ "<leader>fh", desc = "Find Help" },
 	{ "<leader>fw", desc = "Find Text" },
+
 	{ "<leader>g", group = "Git" },
 	{ "<leader>gb", desc = "Branches" },
 	{ "<leader>gc", desc = "Commits" },
 	{ "<leader>gs", desc = "Status" },
-	{ "<leader>h", desc = "No highlight" },
+
 	{ "<leader>l", group = "LSP" },
 	{ "<leader>lD", desc = "Hover diagnostic" },
 	{ "<leader>la", desc = "Action" },
@@ -20,7 +26,4 @@ wk.add({
 	{ "<leader>lf", desc = "Format" },
 	{ "<leader>lr", desc = "Rename" },
 	{ "<leader>ls", desc = "Symbol" },
-	{ "<leader>o", desc = "Git Status" },
-	{ "<leader>w", desc = "Save" },
-	{ "<leader>q", desc = "Close Current Buffer" },
 }, { prefix = "<leader>" })
