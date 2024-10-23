cf09c6fdf359aaae7081acb3e1bd7468a11346f9
GooseOb <gooose.ob@gmail.com>
2024-10-23 02:56:30 +0200

    improve naming

--- lua/commands/JavaCreate.lua
+++ lua/commands/JavaCreate.lua
@@ -28,15 +28,15 @@ end
 -- JavaCreate class/interface name [extends] [implements...]
 vim.api.nvim_create_user_command("JavaCreate", function(opts)
 	local args = vim.split(opts.args, " ")
-	local prefix, name = args[1]:lower(), cap_first(args[2])
+	local type, name = args[1]:lower(), cap_first(args[2])
 	local extends = (args[3] and args[3] ~= "-") and " extends " .. cap_first(args[3]) or ""
 	local implements = #args > 3
-			and prefix ~= "interface"
+			and type ~= "interface"
 			and " implements " .. table.concat(map_mut(slice(args, 4), cap_first), ", ")
 		or ""
 	vim.cmd.edit(name .. ".java")
 	local lines = {
-		"public " .. prefix .. " " .. name .. extends .. implements .. " {",
+		"public " .. type .. " " .. name .. extends .. implements .. " {",
 		"\t",
 		"}",
 	}
@@ -52,12 +52,12 @@ end, {
 		arg_lead = arg_lead:lower()
 
 		if arg_num == 2 then
-			for _, class in ipairs({
+			for _, type in ipairs({
 				"class",
 				"interface",
 			}) do
-				if class:find(arg_lead, 1, true) then
-					table.insert(matches, class)
+				if type:find(arg_lead, 1, true) then
+					table.insert(matches, type)
 				end
 			end
 		elseif arg_num > 3 then
