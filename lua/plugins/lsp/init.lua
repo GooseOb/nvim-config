0b20bf1c87bb84cbed40b5c757254a3f340c300a
GooseOb <gooose.ob@gmail.com>
2024-11-01 19:44:55 +0100

    remove capabilities reassignment

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,6 +1,5 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 for _, server in ipairs(require("plugins.lsp.servers")) do
 	local name = server[1]
