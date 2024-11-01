abd3a3992377a40be944616d49421cce442f1e87
GooseOb <gooose.ob@gmail.com>
2024-11-01 19:03:09 +0100

    ignore eslint if there is no config

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -9,6 +9,13 @@ lint.linters_by_ft = {
 	javascriptreact = eslint,
 }
 
+lint.linters.eslint_d = require("lint.util").wrap(lint.linters.eslint_d, function(diagnostic)
+	if diagnostic.message:find("Error: Could not find config file") then
+		return nil
+	end
+	return diagnostic
+end)
+
 vim.api.nvim_create_autocmd({ "BufRead", "TextChanged" }, {
 	callback = function()
 		lint.try_lint()
