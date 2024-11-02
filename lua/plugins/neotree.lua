b135a6266bcb69d4597985ff2ff318463e607c8e
GooseOb <gooose.ob@gmail.com>
2024-11-02 20:13:03 +0100

    change neotree colors

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,4 +2,9 @@ vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticS
 vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
-require("neo-tree").setup()
+require("neo-tree").setup({
+	popup_border_style = "rounded",
+})
+
+vim.api.nvim_set_hl(0, "NeoTreeFloatTitle", { bg = "NONE", fg = "#938AA9" })
+vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "NONE", fg = "#938AA9" })
