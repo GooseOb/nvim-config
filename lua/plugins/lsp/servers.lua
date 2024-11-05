9a1a84a2753005f258f755acc8d7b2af681599e2
GooseOb <gooose.ob@gmail.com>
2024-11-06 00:35:27 +0100

    update jdtls config

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -1,11 +1,37 @@
-local masonPackagesPath = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/"
+local data_path = vim.fn.stdpath("data")
+local mason_packages_path = data_path .. "/mason/packages"
 
 return {
 	{ "ts_ls" },
 	{ "cssls" },
 	{ "clangd" },
 	{ "emmet_ls" },
-	{ "jdtls" },
+	{
+		"jdtls",
+		{
+			cmd = {
+				"java",
+				"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+				"-Dosgi.bundles.defaultStartLevel=4",
+				"-Declipse.product=org.eclipse.jdt.ls.core.product",
+				"-Dlog.level=ALL",
+				"-Xms1g",
+				"--add-modules=ALL-SYSTEM",
+				"--add-opens",
+				"java.base/java.util=ALL-UNNAMED",
+				"--add-opens",
+				"java.base/java.lang=ALL-UNNAMED",
+				"-jar",
+				vim.fn.glob(mason_packages_path .. "/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+				"-configuration",
+				mason_packages_path
+					.. "/jdtls/config_"
+					.. (vim.loop.os_uname().sysname == "Darwin" and "mac" or "linux"),
+				"-data",
+				data_path .. "/jdtls-workspaces/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"),
+			},
+		},
+	},
 	{ "html" },
 	{
 		"rust_analyzer",
@@ -53,7 +79,7 @@ return {
 			},
 			init_options = {
 				typescript = {
-					tsdk = masonPackagesPath .. "/vue-language-server/node_modules/typescript/lib",
+					tsdk = mason_packages_path .. "/vue-language-server/node_modules/typescript/lib",
 				},
 				vue = {
 					hybridMode = false,
