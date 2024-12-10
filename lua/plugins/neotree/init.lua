565d4f4f363cd7d82cb2d7c1773b2171b8492dc4
GooseOb <gooose.ob@gmail.com>
2024-12-10 16:19:13 +0100

    remove Neotree recursive open

--- lua/plugins/neotree/init.lua
+++ lua/plugins/neotree/init.lua
@@ -19,19 +19,8 @@ return {
 		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
 		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-		local recursive_open = require("plugins.neotree.recursive-open")
-
 		require("neo-tree").setup({
 			popup_border_style = "rounded",
-			filesystem = {
-				window = {
-					mappings = {
-						["Z"] = function(state)
-							recursive_open(state, state.tree:get_node(), 10)
-						end,
-					},
-				},
-			},
 		})
 
 		vim.api.nvim_set_hl(0, "NeoTreeFloatTitle", { bg = "NONE", fg = "#938AA9" })
