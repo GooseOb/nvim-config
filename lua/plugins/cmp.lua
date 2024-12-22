51f42ccc876ab8771f519d1bba2f220dee27947e
GooseOb <gooose.ob@gmail.com>
2024-12-22 15:13:27 +0100

    add copilot to cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,10 @@
 return {
 	"saghen/blink.cmp",
 	event = "InsertEnter",
-	dependencies = "rafamadriz/friendly-snippets",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"giuxtaposition/blink-cmp-copilot",
+	},
 
 	version = "*",
 	---@module 'blink.cmp'
@@ -15,7 +18,59 @@ return {
 		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			providers = {
+				copilot = {
+					name = "copilot",
+					module = "blink-cmp-copilot",
+					score_offset = 100,
+					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Copilot"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
+				},
+			},
+			default = { "lsp", "path", "snippets", "buffer", "copilot" },
+		},
+		appearance = {
+			kind_icons = {
+				Copilot = "",
+				Text = "󰉿",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰒓",
+
+				Field = "󰜢",
+				Variable = "󰆦",
+				Property = "󰖷",
+
+				Class = "󱡠",
+				Interface = "󱡠",
+				Struct = "󱡠",
+				Module = "󰅩",
+
+				Unit = "󰪚",
+				Value = "󰦨",
+				Enum = "󰦨",
+				EnumMember = "󰦨",
+
+				Keyword = "󰻾",
+				Constant = "󰏿",
+
+				Snippet = "󱄽",
+				Color = "󰏘",
+				File = "󰈔",
+				Reference = "󰬲",
+				Folder = "󰉋",
+				Event = "󱐋",
+				Operator = "󰪚",
+				TypeParameter = "󰬛",
+			},
 		},
 	},
 	opts_extend = { "sources.default" },
