c4f03d126ce415c2882651d00981b7f66b0ff196
GooseOb <gooose.ob@gmail.com>
2024-12-27 13:10:00 +0100

    add nvim-highlight-colors to cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,4 +1,3 @@
--- TODO: Add nvim-highlight-colors support when it's available
 return {
 	"saghen/blink.cmp",
 	event = "InsertEnter",
@@ -50,6 +49,40 @@ return {
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 500,
+			},
+			menu = {
+				draw = {
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								local icon = ctx.kind_icon
+								if ctx.item.source_name == "LSP" then
+									local color_item = require("nvim-highlight-colors").format(
+										ctx.item.documentation,
+										{ kind = ctx.kind }
+									)
+									if color_item and color_item.abbr then
+										icon = color_item.abbr
+									end
+								end
+								return icon .. ctx.icon_gap
+							end,
+							highlight = function(ctx)
+								local highlight = "BlinkCmpKind" .. ctx.kind
+								if ctx.item.source_name == "LSP" then
+									local color_item = require("nvim-highlight-colors").format(
+										ctx.item.documentation,
+										{ kind = ctx.kind }
+									)
+									if color_item and color_item.abbr_hl_group then
+										highlight = color_item.abbr_hl_group
+									end
+								end
+								return highlight
+							end,
+						},
+					},
+				},
 			},
 		},
 		appearance = {
