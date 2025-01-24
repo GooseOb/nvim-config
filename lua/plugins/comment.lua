48f9a8cfa8c96e4726014bb3b35cb231a94cd85c
GooseOb <gooose.ob@gmail.com>
2025-01-24 17:29:10 +0100

    make commentstring not lazy

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,7 +3,10 @@ vim.keymap.set({ "n", "v" }, "<leader>/", ":CommentToggle<CR>")
 return {
 	"terrortylor/nvim-comment",
 	cmd = "CommentToggle",
-	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = false,
+	},
 	config = function()
 		require("nvim_comment").setup()
 	end,
