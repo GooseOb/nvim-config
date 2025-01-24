09357b2898e94b7a434c15cc611f65c884d78d21
GooseOb <gooose.ob@gmail.com>
2025-01-24 18:36:15 +0100

    change the comment plugin

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,13 +1,20 @@
-vim.keymap.set({ "n", "v" }, "<leader>/", ":CommentToggle<CR>")
-
 return {
-	"terrortylor/nvim-comment",
-	cmd = "CommentToggle",
-	dependencies = {
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		lazy = false,
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+		},
 	},
-	config = function()
-		require("nvim_comment").setup()
-	end,
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		opts = {
+			enable_autocmd = false,
+		},
+	},
 }
