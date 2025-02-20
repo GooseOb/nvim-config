effebad6778df3888dd089482cb1b1b818d88ff1
GooseOb <gooose.ob@gmail.com>
2025-02-20 20:38:01 +0100

    use native copilot complition instead of blink cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,3 @@
-vim.api.nvim_set_hl(0, "CopilotIcon", {
-	link = "lualine_c_copilot_enabled_normal",
-})
-
 return {
 	"saghen/blink.cmp",
 	event = "InsertEnter",
@@ -51,25 +47,10 @@ return {
 		},
 
 		sources = {
-			providers = {
-				copilot = {
-					name = "copilot",
-					module = "blink-cmp-copilot",
-					score_offset = 100,
-					async = true,
-					transform_items = function(_, items)
-						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-						local kind_idx = #CompletionItemKind + 1
-						CompletionItemKind[kind_idx] = "Copilot"
-						for _, item in ipairs(items) do
-							item.kind = kind_idx
-						end
-						return items
-					end,
-				},
-			},
-			default = { "lsp", "path", "snippets", "buffer", "copilot" },
-			cmdline = {},
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		cmdline = {
+			sources = {},
 		},
 		completion = {
 			accept = { auto_brackets = { enabled = false } },
@@ -118,41 +99,6 @@ return {
 						},
 					},
 				},
-			},
-		},
-		appearance = {
-			kind_icons = {
-				Copilot = "",
-				Text = "󰉿",
-				Method = "󰊕",
-				Function = "󰊕",
-				Constructor = "󰒓",
-
-				Field = "󰜢",
-				Variable = "󰆦",
-				Property = "󰖷",
-
-				Class = "󱡠",
-				Interface = "󱡠",
-				Struct = "󱡠",
-				Module = "󰅩",
-
-				Unit = "󰪚",
-				Value = "󰦨",
-				Enum = "󰦨",
-				EnumMember = "󰦨",
-
-				Keyword = "󰻾",
-				Constant = "󰏿",
-
-				Snippet = "󱄽",
-				Color = "󰏘",
-				File = "󰈔",
-				Reference = "󰬲",
-				Folder = "󰉋",
-				Event = "󱐋",
-				Operator = "󰪚",
-				TypeParameter = "󰬛",
 			},
 		},
 	},
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,10 +1,16 @@
+vim.api.nvim_set_hl(0, "CopilotIcon", {
+	link = "lualine_c_copilot_enabled_normal",
+})
+
 return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
 	event = "InsertEnter",
 	opts = {
-		suggestion = { enabled = false },
-		panel = { enabled = false },
+		suggestion = {
+			enabled = true,
+			auto_trigger = true,
+		},
 		filetypes = {
 			markdown = true,
 		},
