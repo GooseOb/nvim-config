a755bf10e751164de802f0eb0bac68e11bc5b0bd
GooseOb <gooose.ob@gmail.com>
2025-05-18 17:40:20 +0200

    delete commented code

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -68,10 +68,6 @@ return {
 					"bash",
 				},
 			}),
-			-- format_on_save = {
-			-- 	timeout_ms = 1000,
-			-- 	lsp_format = "fallback",
-			-- },
 		})
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			pattern = "*",
