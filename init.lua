3cb29ee94a74b16ed63a6dc091de98d447db456d
GooseOb <gooose.ob@gmail.com>
2025-08-31 19:41:38 +0200

    add emacs-style file variables

--- init.lua
+++ init.lua
@@ -2,3 +2,14 @@ require("core.mappings")
 require("core.plugins")
 require("core.configs")
 require("core.typo")
+
+-- Respect Emacs-style file variables
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+	pattern = "*",
+	callback = function()
+		local mode = vim.fn.getline(1):match("%-%*-%s*mode:%s*([%w_]+)%s*%-%*-")
+		if mode then
+			vim.bo.filetype = mode
+		end
+	end,
+})
