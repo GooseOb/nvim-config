d1952e0a7faf6a0b140ff333d608e0103e90d2af
Edvin Dulko <e.dulko@godeltech.com>
2025-10-20 13:11:02 +0200

    fix kanagawa styles

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,44 @@
 return {
 	"rebelot/kanagawa.nvim",
 	lazy = false,
-	opts = {
-		compile        = true,
-		typeStyle      = { bold = false },
-		functionStyle  = { bold = false },
-		keywordStyle   = { italic = false, bold = false },
-		commentStyle   = { italic = false },
-		statementStyle = { bold = false },
-		colors         = {
-			theme = {
-				all = {
-					ui = {
-						bg_gutter = "none",
+	init = function()
+		local kanagawa = require("kanagawa")
+
+		local no_bold_italic = {
+			italic = false,
+			bold   = false,
+		}
+
+		local highlights = {}
+
+		for k, v in pairs(require("kanagawa.highlights").setup(
+			require("kanagawa.colors").setup({ theme = "wave" }),
+			kanagawa.config
+		)) do
+			if (v.italic or v.bold) then
+				highlights[k] = no_bold_italic
+			end
+		end
+
+		highlights.TabLineFill = { bg = "none" }
+
+		kanagawa.setup({
+			compile = true,
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
 					},
 				},
 			},
-		},
-		transparent    = true,
-		overrides      = function()
-			return {
-				Boolean = { bold = false },
-			}
-		end,
+			transparent = true,
+			overrides = function()
+				return highlights
+			end
 
-	},
-	init = function()
-		require("kanagawa").load("wave")
-		vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#54546D" })
+		})
+		kanagawa.load("wave")
 	end,
 }
